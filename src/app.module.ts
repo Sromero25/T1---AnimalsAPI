53a910ff3666faec5f050f6116fdd4946abdc619
Saulo Romero <saulofco25@gmail.com>
2026-08-31 22:15:35 -0600

    Configuración de app.module e intalación de nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AnimalModule } from './animal/animal.module';
 
 @Module({
-  imports: [AnimalModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AnimalModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
